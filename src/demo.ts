652c3ff7e0f47a1b6c76352f69f9111c852dd6d1
zwalla <tomstlaurent.mail@gmail.com>
2018-11-29 01:48:14 -0500

    update

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,10 @@
 import { CurveLineChart } from "./CurveLineChart";
+import { CurveDoughnutChart } from "./CurveDoughnutChart";
 
 const container = document.getElementById('charts');
 
-const lineChart = new CurveLineChart(container);
+const lineChart = new CurveLineChart(container, {maxDots: 50});
 lineChart.draw([1, 6e18, 1000000000, 1, 3e18, 2000000000], 990000000);
+
+const doughnutChart = new CurveDoughnutChart(container);
+doughnutChart.draw([4,0,0,2,3,1000,3,0,0,2000000000000000000,2000], 1322, 20);
--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,14 @@ export function reduce(dots: number, maxCount: number): number[] {
     for (let i = 1; i <= dots; i++) res.push(i);
     return res;
   }
-  const blockSize = dots / maxCount;
-  const reduced = [];
-  for (let i = 0; i < dots;) {
+  const blockSize = dots / (maxCount - 2);
+  const reduced = [1];
+  reduced.push(average(1, blockSize));
+  for (let i = blockSize, len = dots - blockSize; i < len;) {
     reduced.push(average(i, (i += blockSize)));
   }
+  reduced.push(average(dots - blockSize, dots));
+  reduced.push(dots);
   return reduced;
 }
 
