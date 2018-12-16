d88abf6a5a4af6b9c17e4a4c4a369d1a6601aa09
Pavel B <pavel.b@smartum.pro>
2018-12-16 12:25:18 +0300

    add options for line and ctx

--- lib/CurveLineChart.d.ts
+++ lib/CurveLineChart.d.ts
@@ -1,14 +1,42 @@
 import { ChartOptions } from './ChartOptions';
+interface CtxOptions {
+    shadowColor?: string;
+    shadowBlur?: number;
+    shadowRadius?: number;
+    shadowOffsetX?: number;
+    shadowOffsetY?: number;
+}
+interface LineOptions {
+    fill?: boolean;
+    fillOpacity?: number;
+    pointStyle?: string;
+    borderColor?: string;
+    backgroundColor?: string;
+    lineTension?: number;
+    borderWidth?: number;
+    pointRadius?: number;
+    pointBorderWidth?: number;
+    pointBackgroundColor?: string;
+    pointBorderColor?: string;
+    currentPointRadius?: number;
+    currentPointBorderWidth?: number;
+    currentPointBackgroundColor?: string;
+    currentPointBorderColor?: string;
+}
 export declare class CurveLineChart {
+    private ctxOptions;
+    private lineOptions;
     private chart;
     private chartDatasets;
     private chartLabels;
     private canvas;
     private maxDots;
-    constructor(container: HTMLElement, options?: ChartOptions);
+    constructor(container: HTMLElement, options?: ChartOptions, lineOptions?: LineOptions, ctxOptions?: CtxOptions);
     destroy(): void;
     private getDataset;
     draw(curveParams?: number[], issuedDots?: number): void;
+    private initContext;
     private renderChart;
     private updateChart;
 }
+export {};
