687b7f851fdd927b1b40d201fd966d05c895b09f
Pavel B <pavel.b@smartum.pro>
2019-03-04 20:26:22 +0300

    remove word `ether` from format price

--- src/utils.ts
+++ src/utils.ts
@@ -20,7 +20,7 @@ export function average(start, end): number {
 }
 
 export function formatPrice(wei: number): string {
-  if (wei >= 1e16) return Math.round(wei / 1e15) / 1e3 + ' ether';
+  if (wei >= 1e16) return Math.round(wei / 1e15) / 1e3 + '';
   if (wei >= 1e6) return Math.round(wei / 1e6) / 1e3 + ' gwei';
   return wei + ' wei';
 }
