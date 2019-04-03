90315c0922b532ccdab4f7203b5a4a49a4addd52
Evgeny M <yauhen.m@smartym.co>
2019-04-03 16:42:54 +0300

    bind history chart added

---
+++ src/BondHistoryLineChart.ts
@@ -0,0 +1,205 @@
+import * as Chart from 'chart.js';
+import { reduce } from './utils';
+import { CtxOptions, LineOptions, ChartOptions } from './types';
+
+export class BondHistoryLineChart {
+
+  private ctxOptions: CtxOptions = {
+    shadowColor: '#07c',
+    shadowBlur: 10,
+    shadowRadius: 10,
+    shadowOffsetX: 0,
+    shadowOffsetY: 0,
+  }
+
+  private lineOptions: LineOptions = {
+    fill: true,
+    fillOpacity: 1,
+    pointStyle: 'circle',
+    borderColor: 'rgba(0,120,254,0.75)',
+    backgroundColor: 'rgba(0,120,254,0.5)',
+    lineTension: 0.10,
+    borderWidth: 2,
+    pointBackgroundColor: 'rgba(0,120,254,1)',
+    pointBorderColor: 'rgba(0,120,254,1)',
+    pointRadius: 0,
+    currentPointRadius: 5,
+    pointBorderWidth: 0,
+    currentPointBorderWidth: 3,
+    currentPointBackgroundColor: undefined,
+    currentPointBorderColor: undefined,
+  }
+
+  private chart = null;
+  private chartDatasets = [];
+  private chartLabels = [];
+  private canvas = null;
+  private maxDots: number;
+
+  constructor(container: HTMLElement, options: ChartOptions = {height: 150, width: 300}, lineOptions: LineOptions = {}, ctxOptions: CtxOptions = {}) {
+    this.maxDots = options.maxDots || 300;
+    this.canvas = document.createElement('canvas');
+    if (options.hasOwnProperty('height')) this.canvas.style.height = options.height + 'px';
+    if (options.hasOwnProperty('width')) this.canvas.style.width = options.width + 'px';
+
+    this.ctxOptions = Object.assign(this.ctxOptions, ctxOptions);
+    this.lineOptions = Object.assign(this.lineOptions, lineOptions);
+
+    container.appendChild(this.canvas);
+  }
+
+  public destroy() {
+    if (!this.chart) return;
+    this.chart.destroy();
+  }
+
+  private getDataset(values: number[]): Array<{x: number; y: number}> {
+    return reduce(values.length, this.maxDots).map(x => ({x, y: values[x - 1] / 1e18}));
+  }
+
+  public draw(values: number[] = []) {
+    const dataset = this.getDataset(values);
+    this.chartLabels = dataset.map(data => data.x);
+    const pointPersonalization = {
+      pointRadius: [],
+      pointBorderWidth: [],
+    };
+    dataset.forEach((data, index) => {
+      pointPersonalization.pointRadius.push(this.lineOptions.pointRadius);
+      pointPersonalization.pointBorderWidth.push(this.lineOptions.pointBorderWidth);
+    });
+    this.chartDatasets = [{
+      label: '',
+      data: dataset.map(data => data.y),
+      fill: this.lineOptions.fill,
+      fillOpacity: this.lineOptions.fillOpacity,
+      pointBackgroundColor: this.lineOptions.pointBackgroundColor,
+      pointBorderColor: this.lineOptions.pointBorderColor,
+      pointStyle: this.lineOptions.pointStyle,
+      borderColor: this.lineOptions.borderColor,
+      backgroundColor: this.lineOptions.backgroundColor,
+      lineTension: this.lineOptions.lineTension,
+      pointRadius: pointPersonalization.pointRadius,
+      pointBorderWidth: pointPersonalization.pointBorderWidth,
+      borderWidth: this.lineOptions.borderWidth,
+    }];
+    if (!this.chart) this.renderChart();
+    this.updateChart();
+  }
+
+  private initContext() {
+    const defaultCtxOptions = this.ctxOptions;
+    const initialize = Chart.controllers.line.prototype.initialize;
+    Chart.controllers.line.prototype.initialize = function() {
+      initialize.apply(this, arguments);
+      const ctx = this.chart.chart.ctx;
+      const _stroke = ctx.stroke;
+      ctx.stroke = function() {
+        ctx.save();
+        ctx.shadowColor = defaultCtxOptions.shadowColor;
+        ctx.shadowBlur = defaultCtxOptions.shadowBlur;
+        ctx.shadowRadius = defaultCtxOptions.shadowRadius;
+        ctx.shadowOffsetX = defaultCtxOptions.shadowOffsetX;
+        ctx.shadowOffsetY = defaultCtxOptions.shadowOffsetY;
+        _stroke.apply(this, arguments);
+        ctx.restore();
+      };
+    };
+    return () => {
+      Chart.controllers.line.prototype.initialize = initialize;
+    }
+  }
+
+  private renderChart() {
+    if (this.chart) this.chart.destroy();
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: 'line',
+      options: {
+        elements: {
+          line: {
+            tension: 0, // disables bezier curves
+          }
+        },
+        animation: {
+          duration: 0
+        },
+        layout: {
+          padding: {
+            top: 7,
+            bottom: 0,
+            left: -10
+          },
+        },
+        responsive: false,
+        maintainAspectRatio: false,
+        tooltips: {
+          enabled: true,
+          intersect: false,
+          mode: 'index',
+        },
+        legend: {
+          display: false,
+        },
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                display: false,
+                minRotation: 0,
+                padding: 5,
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineWidth: 0,
+                display: false,
+                drawTicks: false,
+              },
+              scaleLabel: {
+                display: false,
+                labelString: 'Dot number',
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                display: false,
+                beginAtZero: true
+              },
+              gridLines: {
+                drawBorder: false,
+                zeroLineWidth: 0,
+                display: false,
+                drawTicks: true,
+              },
+              scaleLabel: {
+                display: false,
+                labelString: 'Dot price',
+              },
+            },
+          ],
+        },
+      },
+      data: {
+        datasets: [],
+        labels: [],
+      },
+    });
+  }
+
+  private updateChart() {
+    if (!this.chart) return;
+    this.chart.data.datasets = this.chartDatasets;
+    this.chart.data.labels = this.chartLabels;
+
+    // Calculate the max manually
+    const maxY = Math.max.apply(null, this.chartDatasets[0].data) * 2;
+    this.chart.options.scales.yAxes[0].ticks.max = maxY;
+
+    const minX = Math.min.apply(null, this.chartLabels);
+    this.chart.options.scales.xAxes[0].ticks.min = minX;
+    const resetore = this.initContext();
+    this.chart.update();
+    resetore();
+  }
+}
---
+++ src/BondHistorySvgLineChart.ts
@@ -0,0 +1,209 @@
+import { LineOptions, ChartOptions, CtxOptions } from './types';
+import { reduce, formatPrice } from './utils';
+
+export class BondHistorySvgLineChart {
+  private svg: SVGSVGElement;
+  private polyline: SVGPolylineElement;
+  private fill: SVGPolylineElement;
+  private gText: SVGGElement;
+  private textM: SVGTextElement;
+  private textMDots: SVGTSpanElement;
+  private textMPrice: SVGTSpanElement;
+  private coef = 1;
+  private maxX: number;
+  private infoText: SVGRectElement;
+  private values: any;
+
+  private maxDots: number;
+
+  private textColor = '#fff';
+  private textBackground = 'rgba(0, 0, 0, .7)';
+
+  private lineOptions: LineOptions = {
+    fill: true,
+    borderWidth: 2,
+    borderColor: 'rgba(0,120,254,0.75)',
+    backgroundColor: 'rgba(0,120,254,0.5)',
+    pointRadius: 7,
+    pointBackgroundColor: 'rgba(0,120,254,1)',
+  }
+
+  private ctxOptions: CtxOptions = {
+    shadowBlur: 3,
+    shadowRadius: 3,
+    shadowOffsetX: 0,
+    shadowOffsetY: 0,
+  }
+
+  private options: ChartOptions = {
+    height: 150,
+    width: 300,
+    maxDots: 300,
+  }
+
+  private black = false;
+
+  constructor(private container: HTMLElement, options: ChartOptions = {}, lineOptions: LineOptions = {}, ctxOptions: CtxOptions = {}) {
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
+
+    this.lineOptions = Object.assign(this.lineOptions, lineOptions);
+    this.options = Object.assign(this.options, options);
+    this.ctxOptions = Object.assign(this.ctxOptions, ctxOptions);
+
+    const { height, width } = this.options;
+    this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    this.svg.setAttributeNS(null, "viewBox", `0 -${height/2} ${width} ${height}`);
+    this.svg.setAttribute('width', width.toString());
+    this.svg.setAttribute('height', (height + 42).toString());
+
+    const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+    const shadowFilterId = Math.random() + 'shadow';
+    if (this.ctxOptions.shadowRadius) {
+      const shadow = this.getFilter(shadowFilterId);
+      defs.append(shadow);
+    }
+    this.svg.append(defs);
+
+    this.polyline = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+    this.polyline.setAttributeNS(null, 'stroke', this.lineOptions.borderColor);
+    this.polyline.setAttributeNS(null, 'fill', 'none');
+    this.polyline.setAttributeNS(null, 'stroke-width', this.lineOptions.borderWidth.toString());
+    if (this.ctxOptions.shadowRadius) this.polyline.setAttributeNS(null, 'filter', `url(#${shadowFilterId})`);
+    this.fill = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+    this.fill.setAttributeNS(null, 'stroke', 'none');
+    this.fill.setAttributeNS(null, 'fill', this.lineOptions.backgroundColor);
+    this.fill.setAttributeNS(null, 'stroke-width', this.lineOptions.borderWidth.toString());
+
+    this.gText = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    this.gText.setAttributeNS(null, 'font-size', '12');
+    this.gText.setAttributeNS(null, 'font-family', 'sans-serif');
+    this.gText.setAttributeNS(null, 'stroke', 'none');
+    this.gText.setAttributeNS(null, 'text-anchor', 'start');
+    this.gText.setAttributeNS(null, 'fill', this.textColor);
+    this.textM = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    this.textMDots = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
+    this.textMDots.setAttributeNS(null, 'dy', '1em');
+    this.textMPrice = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
+    this.textMPrice.setAttributeNS(null, 'dy', '1em');
+    this.textM.appendChild(this.textMDots);
+    this.textM.appendChild(this.textMPrice);
+    this.textM.style.visibility = 'hidden';
+    this.infoText = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    this.infoText.setAttributeNS(null, 'ry', '5');
+    this.infoText.setAttributeNS(null, 'rx', '5');
+    this.infoText.setAttributeNS(null, 'fill', this.textBackground);
+    this.gText.appendChild(this.textM);
+
+    this.svg.appendChild(this.fill);
+    this.svg.appendChild(this.polyline);
+    this.svg.appendChild(this.infoText);
+    this.svg.appendChild(this.gText);
+
+    this.container.appendChild(this.svg);
+    this.svg.addEventListener('mousemove', this.onMouseMove);
+    this.svg.addEventListener('mouseout', this.onMouseOut);
+  }
+
+  draw(values: any, dots = this.options.maxDots) {
+    const data = this.getDataset(values, dots);
+    this.polyline.setAttributeNS(null, 'points', data.polyline);
+    this.fill.setAttributeNS(null, 'points', data.fill);
+  }
+
+  destroy() {
+    this.svg.removeEventListener('mousemove', this.onMouseMove);
+    this.svg.removeEventListener('mouseout', this.onMouseOut);
+    this.container.removeChild(this.svg);
+  }
+
+  private onMouseMove(e) {
+    this.textM.style.visibility = 'visible';
+    this.infoText.style.visibility = 'visible';
+    const rect = this.polyline.getBoundingClientRect();
+    let deltaX = ((e.clientX - rect.left) > (rect.width / 2)) ? (e.clientX - rect.left) - 120 : (e.clientX - rect.left) + 15;
+    const deltaY = ((e.clientY - rect.top) > (rect.height / 2.5)) ? (e.clientY - rect.top) - 35 : (e.clientY - rect.top) + 15;
+    const coef = (this.maxX - 1) / rect.width;
+    let dots = Math.round((e.clientX - rect.left) * coef);
+    let price = (this.values[dots]) ? this.values[dots] : 'N/A';
+
+    if (
+      price >= 1e7 && price <= 1e15 && (e.clientX - rect.left) > (rect.width / 2)
+      || dots >= 1e7 && dots <= 1e15 && (e.clientX - rect.left) > (rect.width / 2)
+    ) {
+      deltaX -= 35;
+    }
+
+    const priceText = formatPrice(price);
+
+    this.textM.setAttributeNS(null, 'x', deltaX.toString());
+    this.textM.setAttributeNS(null, 'y', deltaY.toString()) ;
+    this.textMDots.setAttributeNS(null, 'x', deltaX.toString());
+    this.textMPrice.setAttributeNS(null, 'x', deltaX.toString());
+    this.textMDots.textContent = `Dot: ${dots + 1}`;
+    this.textMPrice.textContent = `${priceText} ZAP`;
+    const _rect = this.textM.getBoundingClientRect();
+    this.infoText.setAttributeNS(null, 'x', (deltaX - 5).toString());
+    this.infoText.setAttributeNS(null, 'y', (deltaY - 5).toString());
+    this.infoText.setAttributeNS(null, 'width', (_rect.width + 10).toString());
+    this.infoText.setAttributeNS(null, 'height', (_rect.height + 10).toString());
+    return;
+  }
+
+  private onMouseOut() {
+    this.infoText.style.visibility = 'hidden';
+    this.textM.style.visibility = 'hidden';
+  }
+
+  private getFilter(id: string) {
+    const filter = document.createElementNS('http://www.w3.org/2000/svg', 'filter');
+    filter.setAttributeNS(null, 'x', '-40%');
+    filter.setAttributeNS(null, 'y', '-40%');
+    filter.setAttributeNS(null, 'width', '200%');
+    filter.setAttributeNS(null, 'height', '200%');
+    filter.setAttributeNS(null, 'id', id);
+    filter.setAttributeNS(null, 'filterUnits', 'userSpaceOnUse');
+    const feOffset = document.createElementNS('http://www.w3.org/2000/svg', 'feOffset');
+    feOffset.setAttributeNS(null, 'result', 'offOut');
+    feOffset.setAttributeNS(null, 'in', this.black ? 'SourceAlpha' : 'SourceGraphic');
+    feOffset.setAttributeNS(null, 'dx', this.ctxOptions.shadowOffsetX.toString());
+    feOffset.setAttributeNS(null, 'dy', this.ctxOptions.shadowOffsetY.toString());
+    const feGaussianBlur = document.createElementNS('http://www.w3.org/2000/svg', 'feGaussianBlur');
+    feGaussianBlur.setAttributeNS(null, 'result', 'blurOut');
+    feGaussianBlur.setAttributeNS(null, 'in', 'offOut');
+    feGaussianBlur.setAttributeNS(null, 'stdDeviation', this.ctxOptions.shadowRadius.toString());
+    const feBlend = document.createElementNS('http://www.w3.org/2000/svg', 'feBlend');
+    feBlend.setAttributeNS(null, 'in', 'SourceGraphic');
+    feBlend.setAttributeNS(null, 'in2', 'blurOut');
+    feBlend.setAttributeNS(null, 'mode', 'normal');
+    filter.appendChild(feOffset);
+    filter.appendChild(feGaussianBlur);
+    filter.appendChild(feBlend);
+    return filter;
+  }
+
+  private getDataset(values: any, maxDots): any {
+    let maxY = Math.max(...values);
+    this.values = values;
+    this.maxX = values.length;
+
+    const height = this.options.height / 2;
+    const reduced = reduce(this.maxX, maxDots);
+    const coefY = maxY / height;
+    const coefX = this.options.width / (this.maxX - 1);
+    let polyline = [];
+    const fill = [`${0},${height}`];
+    polyline = [];
+
+    reduced.forEach(x => {
+      polyline.push(`${(x-1) * coefX},${height - (values[x-1] / coefY)}`);
+      fill.push(`${(x-1) * coefX},${height - (values[x-1] / coefY)}`);
+    });
+    fill.push(`${this.options.width},${maxY/coefY}`)
+    return {
+      polyline: polyline.join(' '),
+      fill: fill.join(' '),
+      maxY
+    };
+  }
+}
